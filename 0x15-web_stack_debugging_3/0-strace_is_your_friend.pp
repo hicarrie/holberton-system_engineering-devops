a098854574acc906020f040578eb845138efbb0f
Carrie Ybay <carrieybay@gmail.com>
2017-09-10 21:03:19 -0700

    Edit formatting for puppet-lint

--- 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x15-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Changes file name from .phpp to .php
 exec { 'php':
-     command => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
-     path => '/usr/bin:/usr/sbin:/bin',
+  command => 'sed -i "s/.phpp/.php/" /var/www/html/wp-settings.php',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }

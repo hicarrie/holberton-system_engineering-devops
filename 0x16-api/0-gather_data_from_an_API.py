031f7dd6d5ab901bdd4b7c7d2676b1799f00ba6f
Carrie Ybay <carrieybay@gmail.com>
2017-09-18 14:36:23 -0700

    Edit for consistency with other files

--- 0x16-api/0-gather_data_from_an_API.py
+++ 0x16-api/0-gather_data_from_an_API.py
@@ -12,20 +12,20 @@ if __name__ == "__main__":
     employee_id = sys.argv[1]
 
     employee = requests.get(url + "users/" + employee_id)
-    employee_json = employee.json()
+    employee = employee.json()
 
     tasks = requests.get(url + "todos?userId=" + employee_id)
-    tasks_json = tasks.json()
+    tasks = tasks.json()
 
     tasks_done_list = []
-    for item in tasks_json:
+    for item in tasks:
         if item.get('completed') is True:
             tasks_done_list.append(item)
 
     tasks_done = len(tasks_done_list)
-    tasks_total = len(tasks_json)
+    tasks_total = len(tasks)
 
     print("Employee {} is done with tasks({}/{}):".format(
-        employee_json.get('name'), tasks_done, tasks_total))
+        employee.get('name'), tasks_done, tasks_total))
     for item in tasks_done_list:
         print("\t{}".format(item.get('title')))

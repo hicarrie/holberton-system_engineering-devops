9812e39e35daaea552362bf6ebec2aafacfa5f38
Carrie Ybay <carrieybay@gmail.com>
2017-09-18 13:56:35 -0700

    Fix PEP8 formatting

--- 0x16-api/0-gather_data_from_an_API.py
+++ 0x16-api/0-gather_data_from_an_API.py
@@ -8,22 +8,24 @@ import requests
 
 
 if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com/"
     employee_id = sys.argv[1]
 
-    employee = requests.get('https://jsonplaceholder.typicode.com/users/' + employee_id)
+    employee = requests.get(url + "users/" + employee_id)
     employee_json = employee.json()
 
-    tasks = requests.get('https://jsonplaceholder.typicode.com/todos?userId=' + employee_id)
+    tasks = requests.get(url + "todos?userId=" + employee_id)
     tasks_json = tasks.json()
 
     tasks_done_list = []
     for item in tasks_json:
-        if item.get('completed') == True:
+        if item.get('completed') is True:
             tasks_done_list.append(item)
 
     tasks_done = len(tasks_done_list)
     tasks_total = len(tasks_json)
 
-    print("Employee {} is done with tasks({}/{}):".format(employee_json.get('name'), tasks_done, tasks_total))
+    print("Employee {} is done with tasks({}/{}):".format(
+        employee_json.get('name'), tasks_done, tasks_total))
     for item in tasks_done_list:
         print("\t{}".format(item.get('title')))

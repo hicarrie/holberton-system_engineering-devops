71c549d0c60c5e599a5f70ca7db98fec08c9e1e8
Carrie Ybay <carrieybay@gmail.com>
2017-09-18 17:34:55 -0700

    Reorder imports and change method of access dict

--- 0x16-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x16-api/3-dictionary_of_list_of_dictionaries.py
@@ -3,8 +3,8 @@
 Returns information about all employees' TODO list progress in JSON format
 """
 
-import requests
 import json
+import requests
 import sys
 
 
@@ -17,17 +17,13 @@ if __name__ == "__main__":
 
     employee_tasks = {}
     for employee in employees:
-        employee_id = employee['id']
+        employee_id = employee.get('id')
         tasks = requests.get(url + "todos?userId=" + str(employee_id))
         tasks = tasks.json()
 
         tasks_list = []
         for item in tasks:
-            task_dict = {}
-            task_dict.get('task') = item.get('title')
-            task_dict.get('completed') = item.get('completed')
-            task_dict.get('username') = employee.get('username')
-            tasks_list.append(task_dict)
+            tasks_list.append(item)
 
         employee_tasks[employee_id] = tasks_list
 

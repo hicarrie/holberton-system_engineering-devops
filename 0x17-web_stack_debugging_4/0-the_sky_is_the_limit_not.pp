1efd374af11a143f5d4fc5d2e387db43186c61e3
Carrie Ybay <carrieybay@gmail.com>
2017-09-20 09:25:35 -0700

    Edit commands executed

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,11 +1,6 @@
 # Changes limits in /etc/default/nginx and /etc/nginx/nginx.conf
 exec { 'nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/bin/:/usr/sbin:/bin',
-}
-
-exec { 'workerlimit':
-  command => 'sed -i "s/worker_processes 4;/worker_processes 4;\nworker_rlimit_nofile 16384;/" /etc/nginx/nginx.conf',
+  command => 'sed -i "s/15/15000/" /etc/default/nginx',
   path    => '/usr/bin/:/usr/sbin:/bin',
 }
 

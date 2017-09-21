e7668f5487c019a7aa910eaaa5380e9adfecf763
Carrie Ybay <carrieybay@gmail.com>
2017-09-21 13:08:15 -0700

    Combine exec statements and fix path

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,5 @@
 # Changes limits in /etc/default/nginx and /etc/nginx/nginx.conf
 exec { 'nginx':
-  command => 'sed -i "s/15/15000/" /etc/default/nginx',
-  path    => '/usr/bin/:/usr/sbin:/bin',
-}
-
-exec { 'restart':
-  command => 'service nginx restart',
-  path    => '/usr/bin/:/usr/sbin:/bin',
+  command => 'sed -i "s/15/15000/" /etc/default/nginx; service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin',
 }

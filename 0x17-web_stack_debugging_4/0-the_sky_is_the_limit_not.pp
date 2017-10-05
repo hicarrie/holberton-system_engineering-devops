846bd52529a9eb10f9f435f5a00f95a2b50c2e98
Carrie Ybay <carrieybay@gmail.com>
2017-10-05 12:15:46 -0700

    Update path variables

--- 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x17-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,5 +1,5 @@
 # Changes limits in /etc/default/nginx and /etc/nginx/nginx.conf
 exec { 'nginx':
-  command => 'sed -i "s/15/15000/" /etc/default/nginx; service nginx restart',
-  path    => '/usr/bin:/usr/sbin:/bin',
+  command => 'sudo sed -i "s/15/15000/" /etc/default/nginx; sudo service nginx restart',
+  path    => '/usr/bin:/usr/sbin:/bin:/usr/local/sbin:/usr/local/bin:/bin:/sbin',
 }

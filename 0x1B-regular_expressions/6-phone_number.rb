b24836625b6a1849ea81287096f0ca9a18665734
Carrie Ybay <carrieybay@gmail.com>
2017-10-12 09:09:27 -0700

    Edit to only accept exactly 10 digit numbers

--- 0x1B-regular_expressions/6-phone_number.rb
+++ 0x1B-regular_expressions/6-phone_number.rb
@@ -1,4 +1,4 @@
 #!/usr/bin/env ruby
 # Accepts one argument and passes a regular expression matching a 10 digit
 # phone number
-puts ARGV[0].scan(/^\d{10,10}/).join
+puts ARGV[0].scan(/^\d{10}$/).join
